29ac8fcdfd2f7614db34618fbff2e4becc012375
ShreyaR13 <s4.shre@gmail.com>
2024-07-03 06:59:04 -0400

    CSS and App.jsx cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+      // for Job details page
+      // 70/30 ==> left column will be 70 and right column will be 30
+      // 28% (instead of 30) since we are leaving some room (space)
+      gridTemplateColumns: {
+        '70/30' : '70% 28%'
+      }
+    },
   },
   plugins: [],
 }
