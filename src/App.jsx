6bcd56a0b53330b95e377cf6385470d90761b75c
ShreyaR13 <s4.shre@gmail.com>
2024-07-04 14:27:21 -0400

    add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 import MainLayout from './layouts/MainLayout';
 
 // const router = createBrowserRouter(createRoutesFromElements(<Route index element={<h1>My Jobs App</h1>} />));
@@ -64,6 +65,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='*' element={< NotFoundPage />} />
   </Route>
 ));
 
