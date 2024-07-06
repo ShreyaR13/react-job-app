4d2851b7cdb927f557eebe430895bb794d6eb45f
ShreyaR13 <s4.shre@gmail.com>
2024-07-05 21:55:54 -0400

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,12 @@ export default defineConfig({
   // default port is 5173. I like to use 3000 for frontend
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   }
-})
+});
