37029f90451a5fe951724d8e8f7740b6d6ffac4d
ShreyaR13 <s4.shre@gmail.com>
2024-07-05 20:52:56 -0400

    Update spinner color

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -10,7 +10,7 @@ const override = {
 const Spinner = ({ loading }) => {
     return (
         <ClipLoader
-            color='#338ca'
+            color='#4338ca'
             loading={loading}
             cssOverride={override}
             size={150}
