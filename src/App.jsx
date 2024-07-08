7565a83fae7704580b49eb80c6c92fa8fc544aab
ShreyaR13 <s4.shre@gmail.com>
2024-07-08 05:32:40 -0400

    Add Job form

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import MainLayout from './layouts/MainLayout';
 import JobPage, { jobLoader } from './pages/JobPage';
+import AddJob from './pages/AddJob';
 
 // const router = createBrowserRouter(createRoutesFromElements(<Route index element={<h1>My Jobs App</h1>} />));
 
@@ -66,6 +67,7 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/add-job' element={<AddJob />} />
     <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
     <Route path='*' element={< NotFoundPage />} />
   </Route>
